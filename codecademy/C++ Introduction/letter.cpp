bd201bbcefdc1becda74627495a292f661e08d25
rozaliyamarkova <129688789+rozaliyamarkova@users.noreply.github.com>
2025-03-30 15:47:00 +0100

    Create letter.cpp

---
+++ codecademy/C++ Introduction/letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "This is you from the past, specifically 30th March 2025.\n";
+  std::cout << "Hopefully, by the end of this Introduction to C++ you are able to code a program all on your own.\n";
+  std::cout << "Also, I hope you have been saving everything to GitHub. Accountability = Productivity.\n";
+
+}
